d1a235033366f0a077565405d8e76618f03f367b
pahalsinghania <pahalsinghania@gmail.com>
2018-04-19 15:04:17 -0400

    navbar responsive update

--- assets/js/cs/nav.jsx
+++ assets/js/cs/nav.jsx
@@ -7,74 +7,65 @@ import utils from '../util';
 function Nav(props){
 	if(!utils.is_empty(props.token)){
 	return(
-		<nav className="navbar navbar-dark bg-dark navbar-expand mainbar">
+    <nav className="navbar navbar-dark bg-dark navbar-expand ">
       <span className="navbar-brand">
         SMS Client
       </span>
-      <ul className="navbar-nav mr-auto">
+      <ul className="navbar-nav mr-auto hide">
         <NavItem>
-          <NavLink to="/" exact={true} activeClassName="active" className="nav-link">Home</NavLink>
+          <NavLink to="/" exact={true} activeClassName="active" className="nav-link hide">Home</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/inbox" href="#" className="nav-link">Inbox</NavLink>
+          <NavLink to="/inbox" href="#" className="nav-link hide">Inbox</NavLink>
         </NavItem>
         <NavItem>
-          <NavLink to="/newmessage" href="#" className="nav-link">New Message</NavLink>
+          <NavLink to="/newmessage" href="#" className="nav-link hide">New Message</NavLink>
         </NavItem>
     	<NavItem>
-          <NavLink to="/sentmessages" href="#" className="nav-link">Sent Messages</NavLink>
+          <NavLink to="/sentmessages" href="#" className="nav-link hide">Sent Messages</NavLink>
         </NavItem>
       </ul>
-      <span className="navbar-text username">
-                {props.user.name}
-        </span>
-
-      <span className="navbar-text">
-	<a href="javascript:void(0)" className="nav-link" onClick={() => {utils.logout();utils.redirect("/"); } }>Logout</a>
+      <span className="navbar-text hide">
+        {props.user.name}
       </span>
-    </nav>
+	
 
+      <div className = "social drop col-xs-offset-4 col-xs-2 col-sm-offset-7">
+        <button type="button" className="ik fa fa-caret-down" data-toggle="collapse" data-target="#navbar-show">
+          <span className= "sr-only"></span>
+          <span className="icon-bar"></span>
+        </button>
+      </div>
+      
+      <!-- collapsed menu -->
+      <div id = "navbar-show" className= "collapse">
+        <br/>
+        <br/>
 
-	 <!-- menu button for phones -->
-           <div class = "social drop col-xs-offset-4 col-xs-2 col-sm-offset-7">
-             <button type="button" class="ik fa fa-caret-down" data-toggle="collapse" data-target="#navbar">
-                <span class= "sr-only"></span>
-                <span class="icon-bar"></span>
-             </button>
-           </div>
-
-           <!-- collapsed menu -->
-             <div id = "navbar" class= "collapse">
-                  <br/>
-                  <br/>
-    <nav className="navbar navbar-dark bg-dark navbar-expand mainbar">
-      <span className="navbar-brand">
-        SMS Client
-      </span>
-      <ul className="navbar-nav mr-auto">
-        <NavItem>
-          <NavLink to="/" exact={true} activeClassName="active" className="nav-link">Home</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/inbox" href="#" className="nav-link">Inbox</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/newmessage" href="#" className="nav-link">New Message</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink to="/sentmessages" href="#" className="nav-link">Sent Messages</NavLink>
-        </NavItem>
-      </ul>
-      <span className="navbar-text username">
-                {props.user.name}
+        <ul className="navbar-nav mr-">
+          <NavItem>
+            <NavLink to="/" exact={true} activeClassName="active" className="nav-link">Home</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/inbox" href="#" className="nav-link">Inbox</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/newmessage" href="#" className="nav-link">New Message</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/sentmessages" href="#" className="nav-link">Sent Messages</NavLink>
+          </NavItem>
+        </ul>
+        <span className="navbar-text username">
+          {props.user.name}
         </span>
+      </div>
 
       <span className="navbar-text">
         <a href="javascript:void(0)" className="nav-link" onClick={() => {utils.logout();utils.redirect("/"); } }>Logout</a>
       </span>
     </nav>
 
-            </div>
 	);
 	}
 	return (
